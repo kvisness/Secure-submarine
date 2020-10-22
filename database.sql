a560288193d3a0704a806fb3766e98b78e03f132
Kris Visness <kvisness@hbm2.com>
2020-10-21 20:43:32 -0500

    complete hashing and updated readme

--- database.sql
+++ database.sql
@@ -27,3 +27,21 @@ INSERT INTO "secret" ("content", "secrecy_level")
 VALUES ('Admirals Only: Captain Borodin is totally weird.', 13),
 ('Captains Or Above: Lieutenant Ryan is looking fly.', 6),
 ('Lieutenants Or Above: We are heading to the Bahamas.', 3);
+
+DROP TABLE "user";
+
+CREATE TABLE "user"
+(
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "clearance_level" INTEGER NOT NULL DEFAULT 0
+);
+
+INSERT INTO "user"
+    ("username", "password", "clearance_level")
+VALUES
+    ('Admiral Greer', '$2b$10$p5Wkte33hlOBOcUtJie6H.PnCvk8v.KjZspVoAFtT7g5v5xK.EXVG', 18),
+    ('Captain Borodin', '$2b$10$p5Wkte33hlOBOcUtJie6H.ZIgFjzr4zY8FItxC8gZyqIWD5gYmL0m', 10),
+    ('Lieutenant Nguyen', '$2b$10$p5Wkte33hlOBOcUtJie6H.vaUd5ikB1LWCbVZAA87BR63NiDorn1C', 4),
+    ('Lieutenant Ryan', '$2b$10$p5Wkte33hlOBOcUtJie6H.PnCvk8v.KjZspVoAFtT7g5v5xK.EXVG', 4);
